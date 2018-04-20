178aeeecf51e4f18eb731e50fbb8bb4d881665c1
n3op2 <n3op2@n3-lap>
2018-04-20 20:39:16 +0100

    CORS ADDED and tested. Needs tidying

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			messageOfGuest: ''
+		};
+		this.handleMessageOfGuest = this.handleMessageOfGuest.bind(this);
+	}
+	handleMessageOfGuest(event) {
+		this.setState({ messageOfGuest: event.target.value });
+	}
 	state = {
 		response: '',
 		res_arg2: ''
@@ -19,13 +30,34 @@ class App extends Component {
 		const body = await response.json();
 		return body;
 	};
+	addToDb = event => {
 
+		event.preventDefault();
+		this.setState({ messageOfGuest: event.target.value });
+		axios.post('http://localhost:3010/api/newUserRequest', {
+			messageOfGuest: 'asasd',		
+		})
+		.then(response => {
+			console.log(response, 'added');
+		});
+		this.setState({
+			messageOfGuest: '',
+		});
+	};
   	render() {
     	return (
 		<div>
 			<h1>Sample Cloud</h1>
 			<h1>{this.state.response}</h1>
 			<p>{this.state.res_arg2}</p>
+			<h1>{this.state.messageOfGuest}</h1>
+			<input type='text'
+				name='messageOfGues'
+				value={this.state.messageOfGues}
+				onChange={this.handleMessageOfGuest} />
+			<button type='submit' onClick={this.addToDb}>
+			Submit 
+			</button>
 		</div>
 	    );
 	  }
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
+const cors = require('cors');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/api/newUserRequest', (req, res) => {
+	var data = req.body.messageOfGuest;
+	console.log('received from react: ' + data);
+	res.redirect('/home');
+	
+});
 
 app.get('/home', (req, res) => {
 	var obj = {
