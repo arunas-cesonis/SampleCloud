bcd9035542d6fb3e18c0e5a329b1c998c1b48c70
n3op2 <n3op2@n3-lap>
2018-04-28 08:13:51 +0100

    Menu updated using withRouter, still some work needed to be done.

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,90 +1,40 @@
 import React, { Component } from 'react';
 //Using Link for updating URL, Content.jsx will pick them up al will load the req cont
-import { Link } from 'react-router-dom';
-import '../css/menu.css';
+import { Link, withRouter} from 'react-router-dom';
 import classNames from 'classnames';
+import MenuItem from './MenuItem.jsx';
 
 class Menu extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			homeActive: true,
-			aboutActive: false,
-			browseActive: false,
-			uploadActive: false,
-		}
-	}
-	// TO Find a better way of achieving this.
-	handleButton = (arg, event) => {
-		if(arg === 'home'){
-			this.setState({
-				homeActive: true,
-				aboutActive: false,
-				browseActive: false,
-				uploadActive: false,
-			});
-		} else if(arg === 'about'){
-			this.setState({
-				homeActive: false,
-				aboutActive: true,
-				browseActive: false,
-				uploadActive: false,
-			});
-		} else if(arg === 'browse'){
-			this.setState({
-				homeActive: false,
-				aboutActive: false,
-				browseActive: true,
-				uploadActive: false,
-			});
-		} else if(arg === 'upload'){
-			this.setState({
-				homeActive: false,
-				aboutActive: false,
-				browseActive: false,
-				uploadActive: true,
-			});
 		}
 	}
 	
 	render() {
-		let navHomeClass = classNames({
-			'navItem': true,
-			'active': this.state.homeActive,
-		});
-		let navAboutClass = classNames({
-			'navItem': true,
-			'active': this.state.aboutActive,
-		});
-		let navBrowseClass = classNames({
-			'navItem': true,
-			'active': this.state.browseActive,
-		});
-		let navUploadClass = classNames({
-			'navItem': true,
-			'active': this.state.uploadActive,
-		});
+        // Get the current location of the APP so I can check against <Link> path
+        const currentLocation = this.props.location.pathname;
+        console.log(currentLocation);
+		const navItems = [
+			{ path: '/', name: 'Home' },
+			{ path: '/about', name: 'About' },
+			{ path: '/browse', name: 'Browse' },
+			{ path: '/upload', name: 'Upload' },
+		]
+        // To move Menu item into a new file/component
 		return (
-                	<ul>
-				<li><Link className={navHomeClass} 
-						onClick={this.handleButton.bind(this, 'home')}
-						to='/'
-					>Home</Link></li>
-				<li><Link className={navAboutClass} 
-						onClick={this.handleButton.bind(this, 'about')}
-						to='/about'
-					>About</Link></li>
-				<li><Link className={navBrowseClass} 
-						onClick={this.handleButton.bind(this, 'browse')}
-						to='/browse'
-					>Browse</Link></li>
-				<li><Link className={navUploadClass} 
-						onClick={this.handleButton.bind(this, 'upload')}
-						to='/upload'
-					>Upload</Link></li>
+        	<ul>
+			{navItems.map((element, i) => 
+                <li key={i}>
+                    <Link className={classNames('navItem', { active: currentLocation === element.path})} to={element.path}>{element.name}</Link>
+                </li>
+			)}
+
+				<MenuItem name='Test' to='/' />
 			</ul>
 		);
 	}
 }
 
-export default Menu;
+//In oder to use withRouter the component has to wrapped in it
+export default withRouter(Menu);
