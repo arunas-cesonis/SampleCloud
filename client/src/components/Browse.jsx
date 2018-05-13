85dc30842d60ba395098d0caefe7479457e3d1cb
n3op2 <n3op2@n3-lap>
2018-05-13 09:39:46 +0100

    Saving before some mayor changes.

--- client/src/components/Browse.jsx
+++ client/src/components/Browse.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import UsersList from './UsersList.jsx';
+import FilesList from './FilesList.jsx';
 import '../css/browse.css';
 
 class Browse extends Component {
+    constructor(props){
+        super(props);
+    }
+
     render() {
         return (
             <div className='br_main'>
                 <h1>Browse</h1>
-                <UsersList />
+                <UsersList 
+                />
+                <FilesList 
+                />
             </div>
         );
     }
